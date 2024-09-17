46345c840ef86cdcf035956ca2b97f6a1b963182
aminnaz93 <Aminnazari040@hotmail.com>
2024-09-17 13:26:47 +0200

    vs

--- main.c
+++ main.c
@@ -4,23 +4,5 @@ int main(){
 
     printf("HElLO\n");
 
-    for(int i = 0; i < 10; i++){
-        printf("%d\n", i);
-    }
-
-
-    int x = 20;
-    int y = 30;
-
-    if(x == y){
-        printf("HEllo");
-    }
-    else if(x > y){
-        printf("BYe bye");
-    }
-    else{
-        printf("jajajajaj");
-    }
-
     return 0;
 }
