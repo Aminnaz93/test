8ffd15f64da9c90b517d7ba3de322b71ae49ec60
aminnaz93 <Aminnazari040@hotmail.com>
2024-09-17 13:15:32 +0200

    Another commit

--- main.c
+++ main.c
@@ -9,5 +9,18 @@ int main(){
     }
 
 
+    int x = 20;
+    int y = 30;
+
+    if(x == y){
+        printf("HEllo");
+    }
+    else if(x > y){
+        printf("BYe bye");
+    }
+    else{
+        printf("jajajajaj");
+    }
+
     return 0;
 }
