8a12957989817762ce1e73668765d86bb63beb3b
aminnaz93 <Aminnazari040@hotmail.com>
2024-09-17 13:34:23 +0200

    da

--- main.c
+++ main.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+struct myStudent {
+    int age;
+    char name[50]; // Använd en array för sträng
+};
 
+int main() {
     printf("HElLO\n");
 
-    for(int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++) {
         printf("%d\n", i);
     }
-
 
     int x = 20;
     int y = 30;
 
-    if(x == y){
-        printf("HEllo");
+    if (x == y) {
+        printf("HEllo\n");
+    } else if (x > y) {
+        printf("BYe bye\n");
+    } else {
+        printf("jajajajaj\n");
     }
-    else if(x > y){
-        printf("BYe bye");
-    }
-    else{
-        printf("jajajajaj");
-    }
+
+    struct myStudent student1;
+    student1.age = 20;
+    strcpy(student1.name, "Amin"); // Kopiera strängen till namnet
+
+    // Skriva ut medlemsvariabler
+    printf("Age: %d\n", student1.age);
+    printf("Name: %s\n", student1.name);
 
     return 0;
 }
